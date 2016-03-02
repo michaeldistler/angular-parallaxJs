3c241e38ac57c2304044c4a08116f317f97b9a06
Michael Distler <michaeldistler@gmail.com>
2016-03-01 19:28:04 -0500

    Initial commit of Angullax (horrible name, I know)

---
+++ README.md
@@ -0,0 +1,7 @@
+<h3>Angullax - The one stop shop for all AngularJS Parallax needs</h3>
+
+<p>
+  <b>Angullax</b> is an AngularJS module with a goal to prove all the
+  parallax functionality you could possibly dream of. 
+</p>
+
---
+++ app.js
@@ -0,0 +1,63 @@
+var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.name = 'World';
+});
+
+app.directive('parallax', ["$document", "$window", function($document, $window){
+  return {
+    restrict: 'A',
+    scope: {
+      paratype: '@',
+      move: '@',
+      startX: '@x',
+      startY: '@y'
+    },
+    link: function (scope, element, attrs){
+      var startX = parseInt(scope.startX), startY = parseInt(scope.startY), x = 0, y = 0;
+      var moveBy = scope.move, type = scope.paratype;
+      
+      element.css({
+        position: 'absolute',
+        left: startX + "px",
+        top: startY + "px",
+      });
+    
+      setInterval(function printStyle() {
+      //  console.log(element[0].style.cssText);
+      //  console.log("X: " + element[0].style.left, "Y: " + startY);
+      },10000);
+      
+      function mouseMove(event) {
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
+        
+        x = moveBy * (0.5 * $window.innerWidth - mouseX) + startX;
+        
+        element.css({
+          left: x + 'px',
+        });
+
+      }
+  
+      function scrollMove(){
+        var scrollY = $document[0].body.scrollTop;
+        console.log(scrollY);
+        var y = startY - (moveBy * scrollY);
+        console.log(element[0].style.top, "y: " + y);
+        element.css({
+          top: y + 'px',
+        });
+      }
+    
+      if(type === "mousemove") {
+        $document.on("mousemove", mouseMove);
+      }else if(type == "pagescroll") {
+        $document.on("scroll", scrollMove);
+      }
+    }
+    
+  };
+
+}]);
+
